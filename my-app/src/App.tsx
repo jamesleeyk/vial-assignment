6af39a888bc94a32652edaf1eb17256a72747456
James Lee <jameslee0415@hotmail.com>
2024-03-11 01:57:59 +0900

    add styling and responsive breakpoints to table

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -17,35 +17,43 @@ function App() {
   }, [])
 
   return (
-    <div className="main">
-      <table className='main__table'>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Gender</th>
-            <th>Age</th>
-            <th>Diagnosis Date</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            data.map((item: any, index: number) => {
-              return (
-              <tr key={index}>
-                <td>{item.id}</td> 
-                <td>{item.name}</td> 
-                <td>{item.gender}</td> 
-                <td>{item.age}</td> 
-                <td>{item.diagnosisDate}</td> 
-                <td>{item.status}</td>  
+    <body className='body'>
+      <main className="table">
+        <section className='table__header'>
+          Search
+        </section>
+        <section className="table__body">
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Gender</th>
+                <th>Age</th>
+                <th>Diagnosis Date</th>
+                <th>Status</th>
               </tr>
-              )})
-          }
-        </tbody>
-      </table>
-    </div>
+            </thead>
+            <tbody>
+              {
+                data.map((item: any, index: number) => {
+                  return (
+                  <tr key={index}>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.gender}</td>
+                    <td>{item.age}</td>
+                    <td>{item.diagnosisDate}</td>
+                    <td>{item.status}</td>
+                  </tr>
+                  )})
+              }
+            </tbody>
+          </table>
+        </section>
+      </main>
+    </body>
+   
   );
 }
 
